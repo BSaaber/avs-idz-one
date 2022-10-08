ed653a2019b6f8050ce28edc6bfc18f15ea56848
Max <66118806+BSaaber@users.noreply.github.com>
2022-10-08 16:48:09 +0300

    add comments to base_with_funcs.s

--- base_with_funcs.s
+++ base_with_funcs.s
@@ -1,3 +1,5 @@
+# подробные комментарии к ассемблерному коды были приведены в первой ассемблероной программе
+# здесь и далее прокомментированы будут лишь общие комментарии к коду, а также комментарии к новым приемам
 	.file	"base_with_funcs.c"
 	.intel_syntax noprefix
 	.text
@@ -7,13 +9,16 @@
 	.text
 	.globl	print_array
 	.type	print_array, @function
+
+
+# функция print_array
 print_array:
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 32
-	mov	QWORD PTR -24[rbp], rdi
-	mov	DWORD PTR -28[rbp], esi
-	mov	DWORD PTR -4[rbp], 0
+	mov	QWORD PTR -24[rbp], rdi # по смещению -24 от rbp на стеке зраним переменную vals ( значение передано через регистр rdi )
+	mov	DWORD PTR -28[rbp], esi # по смещению -28 от rbp на стеке храним переменную size ( значение передано через регистр esi )
+	mov	DWORD PTR -4[rbp], 0 # по смещению -4 от rbp на стеке храним переменную i (итератор цикла)
 	jmp	.L2
 .L3:
 	mov	rax, QWORD PTR -24[rbp]
@@ -30,10 +35,10 @@ print_array:
 	mov	eax, DWORD PTR -4[rbp]
 	cmp	eax, DWORD PTR -28[rbp]
 	jl	.L3
-	nop
-	nop
+	nop # ничего не делаем
+	nop # ничего не делаем
 	leave
-	ret
+	ret # функция типа void, возвращаемого значения нет
 	.size	print_array, .-print_array
 	.section	.rodata
 .LC1:
@@ -41,15 +46,18 @@ print_array:
 	.text
 	.globl	insert_A_and_count_B_size
 	.type	insert_A_and_count_B_size, @function
+
+
+# функция insert_A_and_count_B_size
 insert_A_and_count_B_size:
 	push	rbp
 	mov	rbp, rsp
 	sub	rsp, 32
-	mov	QWORD PTR -24[rbp], rdi
-	mov	DWORD PTR -28[rbp], esi
-	mov	DWORD PTR -32[rbp], edx
-	mov	DWORD PTR -8[rbp], 0
-	mov	DWORD PTR -4[rbp], 0
+	mov	QWORD PTR -24[rbp], rdi # по смещению -24 от rbp на стеке храним переменную vals ( значение передано через регистр rdi )
+	mov	DWORD PTR -28[rbp], esi # по смещению -28 от rbp на стеке храним переменную size ( значение передано через регистр esi )
+	mov	DWORD PTR -32[rbp], edx # по смещению -32 от rbp на стеке храним переменную divider ( значение передано через регистр edx )
+	mov	DWORD PTR -8[rbp], 0 # по смещению -8 от rbp на стеке храним переменную answer
+	mov	DWORD PTR -4[rbp], 0 # по смещению -4 от rbp на стеке храним переменную i ( итератор цикла )
 	jmp	.L5
 .L7:
 	mov	rax, QWORD PTR -24[rbp]
@@ -74,19 +82,22 @@ insert_A_and_count_B_size:
 	mov	eax, DWORD PTR -4[rbp]
 	cmp	eax, DWORD PTR -28[rbp]
 	jl	.L7
-	mov	eax, DWORD PTR -8[rbp]
+	mov	eax, DWORD PTR -8[rbp] # помещаем возвращаемое значение в регистр eax (через этот регистр передаем возвращаемое значение)
 	leave
-	ret
+	ret # выход из функции, возвращаемое значение сохранено на регистре eax
 	.size	insert_A_and_count_B_size, .-insert_A_and_count_B_size
 	.globl	fill_B
 	.type	fill_B, @function
+
+
+
 fill_B:
 	push	rbp
 	mov	rbp, rsp
-	mov	QWORD PTR -24[rbp], rdi
-	mov	QWORD PTR -32[rbp], rsi
-	mov	DWORD PTR -36[rbp], edx
-	mov	DWORD PTR -40[rbp], ecx
+	mov	QWORD PTR -24[rbp], rdi # по смещению -24 от rbp на стеке храним переменную A ( значение передано через регистр rdi )
+	mov	QWORD PTR -32[rbp], rsi # по смещению -32 от rbp на стеке храним переменную B ( значение передано через регистр rsi )
+	mov	DWORD PTR -36[rbp], edx # по смещению -36 от rbp на стеке храним переменную size_A ( значение передано через регистр edx )
+	mov	DWORD PTR -40[rbp], ecx # по смещению -40 от rbp на стеке храним переменную divider ( значение передано через регистр ecx )
 	mov	DWORD PTR -4[rbp], 0
 	jmp	.L10
 .L12:
@@ -113,7 +124,7 @@ fill_B:
 	nop
 	nop
 	pop	rbp
-	ret
+	ret # функция типа void, ничего не возвращает
 	.size	fill_B, .-fill_B
 	.section	.rodata
 .LC2:
@@ -142,12 +153,12 @@ main:
 	mov	rdi, rax
 	call	malloc@PLT
 	mov	QWORD PTR -24[rbp], rax
-	mov	edx, DWORD PTR -32[rbp]
+	mov	edx, DWORD PTR -32[rbp] # передаем divider
 	mov	ecx, DWORD PTR -36[rbp]
 	mov	rax, QWORD PTR -24[rbp]
-	mov	esi, ecx
-	mov	rdi, rax
-	call	insert_A_and_count_B_size
+	mov	esi, ecx # передаем n (size)
+	mov	rdi, rax # передаем array (vals)
+	call	insert_A_and_count_B_size  # вызов функции insert_A_and_count_B_size
 	mov	DWORD PTR -28[rbp], eax
 	mov	eax, DWORD PTR -28[rbp]
 	cdqe
@@ -155,17 +166,17 @@ main:
 	mov	rdi, rax
 	call	malloc@PLT
 	mov	QWORD PTR -16[rbp], rax
-	mov	ecx, DWORD PTR -32[rbp]
-	mov	edx, DWORD PTR -36[rbp]
-	mov	rsi, QWORD PTR -16[rbp]
-	mov	rax, QWORD PTR -24[rbp]
-	mov	rdi, rax
-	call	fill_B
+	mov	ecx, DWORD PTR -32[rbp] # передаем divider
+	mov	edx, DWORD PTR -36[rbp] # передаем n (size_A)
+	mov	rsi, QWORD PTR -16[rbp] # передаем result_array (B)
+	mov	rax, QWORD PTR -24[rbp] 
+	mov	rdi, rax # передаем array (A)
+	call	fill_B                     # вызов функции fill_B
 	mov	edx, DWORD PTR -28[rbp]
 	mov	rax, QWORD PTR -16[rbp]
-	mov	esi, edx
-	mov	rdi, rax
-	call	print_array
+	mov	esi, edx # передаем result_array_len (size)
+	mov	rdi, rax # передаем result_array (vals)
+	call	print_array                # вызов функции print_array
 	mov	rax, QWORD PTR -24[rbp]
 	mov	rdi, rax
 	call	free@PLT
